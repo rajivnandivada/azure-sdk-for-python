ca0148d882137ee6382946b2fdf10bbd8fd5d129
yijxie <yijun.xie@micrsofot.com>
2019-06-24 00:01:35 -0700

    fix test errors

--- sdk/eventhub/azure-eventhubs/tests/test_iothub_receive.py
+++ sdk/eventhub/azure-eventhubs/tests/test_iothub_receive.py
@@ -17,6 +17,7 @@ def test_iothub_receive_sync(iot_connection_str, device_id):
     client = EventHubClient.from_connection_string(iot_connection_str, network_tracing=False)
     receiver = client.create_consumer(consumer_group="$default", partition_id="0", event_position=EventPosition("-1"), operation='/messages/events')
     receiver._open()
+    receiver.close()
     try:
         partitions = client.get_properties()
         assert len(partitions["partition_ids"]) >= 2
